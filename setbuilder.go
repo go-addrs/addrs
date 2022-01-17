e20876d8b8b15f4cebc1d96323b2703c407c5f0d
Carl Baldwin <carl@ecbaldwin.net>
2022-01-16 20:59:46 -0700

    implement insert and remove on set builder

--- setbuilder.go
+++ setbuilder.go
@@ -10,7 +10,22 @@ func (me *SetBuilder) Set() Set {
 	return Set{trie: me.trie}
 }
 
+// Insert inserts the given address into the set
+func (me *SetBuilder) Insert(addr Addr) {
+	me.InsertPrefix(ipToKey(addr))
+}
+
 // InsertPrefix inserts the given prefix (all of its addreses) into the set
 func (me *SetBuilder) InsertPrefix(prefix Prefix) {
 	me.trie = me.trie.Insert(prefix)
+}
+
+// Remove inserts the given address into the set
+func (me *SetBuilder) Remove(addr Addr) {
+	me.RemovePrefix(ipToKey(addr))
+}
+
+// RemovePrefix inserts the given prefix (all of its addreses) into the set
+func (me *SetBuilder) RemovePrefix(prefix Prefix) {
+	me.trie = me.trie.Remove(prefix)
 }
