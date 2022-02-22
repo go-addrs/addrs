0b4f87b93c0528f7c05989d71fcb91ccfff9855a
Josh Vasilevsky <jvasilevsky@digitalocean.com>
2022-02-22 15:37:22 -0500

    add basic insert and match
    These are the most basic operations on the trie

--- ipv6/node.go
+++ ipv6/node.go
@@ -1,5 +1,9 @@
 package ipv6
 
+import (
+	"fmt"
+)
+
 type trieNode struct {
 	Prefix   Prefix
 	Data     interface{}
@@ -14,6 +18,13 @@ func intMin(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func intMax(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
 }
 
 // contains is a helper which compares to see if the shorter prefix contains the
@@ -86,6 +97,37 @@ func compare(a, b Prefix) (result int, reversed bool, common uint32, child int) 
 	return
 }
 
+func (me *trieNode) mutate(mutator func(*trieNode)) *trieNode {
+	if me == nil {
+		return nil
+	}
+
+	mutator(me)
+
+	numNodes := me.children[0].NumNodes() + me.children[1].NumNodes()
+	height := 1 + intMax(me.children[0].height(), me.children[1].height())
+
+	me.size = uint32(numNodes)
+	me.h = uint16(height)
+	if me.isActive {
+		me.size++
+	}
+	return me
+}
+
+func (me *trieNode) copyMutate(mutator func(*trieNode)) *trieNode {
+	if me == nil {
+		return nil
+	}
+	doppelganger := &trieNode{}
+	*doppelganger = *me
+	mutated := doppelganger.mutate(mutator)
+	if *mutated == *me {
+		return me
+	}
+	return mutated
+}
+
 type comparator func(a, b interface{}) bool
 
 // Equal returns true if all of the entries are the same in the two data structures
@@ -112,4 +154,198 @@ func (me *trieNode) Equal(other *trieNode, eq comparator) bool {
 	default:
 		return true
 	}
+}
+
+// Match returns the existing entry with the longest prefix that fully contains
+// the prefix given by the key argument or nil if none match.
+//
+// "contains" means that the first "length" bits in the entry's key are exactly
+// the same as the same number of first bits in the given search key. This
+// implies the search key is at least as long as any matching node's prefix.
+//
+// Some examples include the following ipv4 and ipv6 matches:
+//     10.0.0.0/24 contains 10.0.0.0/24, 10.0.0.0/25, and 10.0.0.0/32
+//     2001:cafe:beef::/64 contains 2001:cafe:beef::a/124
+//
+// "longest" means that if multiple existing entries in the trie match the one
+// with the longest length will be returned. It is the most specific match.
+func (me *trieNode) Match(searchKey Prefix) *trieNode {
+	if me == nil {
+		return nil
+	}
+
+	nodeKey := me.Prefix
+	if searchKey.length < nodeKey.length {
+		return nil
+	}
+
+	matches, exact, _, child := contains(nodeKey, searchKey)
+	if !matches {
+		return nil
+	}
+
+	if !exact {
+		if better := me.children[child].Match(searchKey); better != nil {
+			return better
+		}
+	}
+
+	if !me.isActive {
+		return nil
+	}
+
+	return me
+}
+
+// NumNodes returns the number of entries in the trie
+func (me *trieNode) NumNodes() int64 {
+	if me == nil {
+		return 0
+	}
+	return int64(me.size)
+}
+
+// height returns the maximum height of the trie.
+func (me *trieNode) height() int {
+	if me == nil {
+		return 0
+	}
+	return int(me.h)
+}
+
+// isValid returns true if the tree is valid
+// this method is only for unit tests to check the integrity of the structure
+func (me *trieNode) isValid() bool {
+	return me.isValidLen(0)
+}
+
+func (me *trieNode) isValidLen(minLen uint32) bool {
+	if me == nil {
+		return true
+	}
+	left, right := me.children[0], me.children[1]
+	size := me.size
+	if me.isActive {
+		size--
+	} else {
+		if left == nil || right == nil {
+			// Any child node should have been pulled up since this node isn't active
+			return false
+		}
+	}
+	if size != uint32(left.NumNodes()+right.NumNodes()) {
+		return false
+	}
+	if me.h != 1+uint16(uint16(intMax(left.height(), right.height()))) {
+		return false
+	}
+	if me.Prefix.length < minLen {
+		return false
+	}
+	return left.isValidLen(me.Prefix.length+1) && right.isValidLen(me.Prefix.length+1)
+}
+
+// Insert is the public form of insert(...)
+func (me *trieNode) Insert(key Prefix, data interface{}) (newHead *trieNode, err error) {
+	return me.insert(&trieNode{Prefix: key, Data: data}, insertOpts{insert: true})
+}
+
+type insertOpts struct {
+	insert bool
+	eq     comparator
+}
+
+// insert adds a node into the trie and return the new root of the trie. It is
+// important to note that the root of the trie can change. If the new node
+// cannot be inserted, nil is returned.
+func (me *trieNode) insert(node *trieNode, opts insertOpts) (newHead *trieNode, err error) {
+	if me == nil {
+		if !opts.insert {
+			return me, fmt.Errorf("the key doesn't exist to update")
+		}
+		node = node.mutate(func(n *trieNode) {
+			n.isActive = true
+		})
+		return node, nil
+	}
+
+	// Test containership both ways
+	result, reversed, common, child := compare(me.Prefix, node.Prefix)
+	switch result {
+	case compareSame:
+		// They have the same key
+		if me.isActive {
+			return me, fmt.Errorf("a node with that key already exists")
+		}
+		if !me.isActive && !opts.insert {
+			return me, fmt.Errorf("the key doesn't exist to update")
+		}
+		return node.mutate(func(n *trieNode) {
+			if me.isActive && opts.eq(me.Data, node.Data) {
+				node.Data = me.Data
+			}
+			n.children = me.children
+			n.isActive = true
+		}), nil
+
+	case compareContains:
+		// Trie node's key contains the new node's key. Insert it recursively.
+		newChild, err := me.children[child].insert(node, opts)
+		if err != nil {
+			return me, err
+		}
+		newNode := me.copyMutate(func(n *trieNode) {
+			n.children[child] = newChild
+		})
+		return newNode, nil
+
+	case compareIsContained:
+		// New node's key contains the trie node's key. Insert new node as the parent of the trie.
+		if !opts.insert {
+			return me, fmt.Errorf("the key doesn't exist to update")
+		}
+		node = node.mutate(func(n *trieNode) {
+			n.children[child] = me
+			n.isActive = true
+		})
+		return node, nil
+
+	case compareDisjoint:
+		// Keys are disjoint. Create a new (inactive) parent node to join them side-by-side.
+		var newChild *trieNode
+		newChild, err := newChild.insert(node, opts)
+		if err != nil {
+			return me, err
+		}
+
+		var children [2]*trieNode
+
+		if (child == 1) != reversed { // (child == 1) XOR reversed
+			children[0], children[1] = me, newChild
+		} else {
+			children[0], children[1] = newChild, me
+		}
+
+		newNode := &trieNode{
+			Prefix: Prefix{
+				addr: Address{
+					ui: me.Prefix.addr.ui.and(uint128{0xffffffffffffffff, 0xffffffffffffffff}.rightShift(int(common)).complement()), // zero out bits not in common
+				},
+				length: common,
+			},
+			children: children,
+		}
+		newNode.mutate(func(n *trieNode) {})
+		return newNode, nil
+	}
+	panic("unreachable code")
+}
+
+// active returns whether a node represents an active prefix in the tree (true)
+// or an intermediate node (false). It is safe to call on a nil pointer.
+func (me *trieNode) active() bool {
+	if me == nil {
+		return false
+	}
+	return me.isActive
 }
--- ipv6/node_test.go
+++ ipv6/node_test.go
@@ -1,6 +1,7 @@
 package ipv6
 
 import (
+	"fmt"
 	"testing"
 	"unsafe"
 
@@ -45,6 +46,454 @@ func TestStructSizes(t *testing.T) {
 		),
 		nodeSize,
 	)
+}
+
+func TestMatchNilTrie(t *testing.T) {
+	var trie *trieNode
+
+	key := Prefix{}
+	assert.Nil(t, trie.Match(key))
+}
+
+func TestMatchZeroLength(t *testing.T) {
+	var trie *trieNode
+
+	trie, err := trie.Insert(Prefix{
+		Address{uint128{0, 0}},
+		0,
+	}, nil)
+	assert.Nil(t, err)
+	assert.True(t, trie.active())
+	assert.True(t, trie.isValid())
+	assert.Equal(t, 1, trie.height())
+
+	assert.Equal(t, trie, trie.Match(Prefix{
+		_a("2001::"),
+		0,
+	}))
+}
+
+func TestNoMatchTooBroad(t *testing.T) {
+	var trie *trieNode
+
+	trie, err := trie.Insert(Prefix{
+		_a("10::"),
+		24,
+	}, nil)
+	assert.Nil(t, err)
+	assert.True(t, trie.active())
+	assert.True(t, trie.isValid())
+	assert.Equal(t, 1, trie.height())
+
+	assert.Nil(t, trie.Match(Prefix{
+		_a("10::"),
+		23,
+	}))
+}
+
+func TestNoMatchPrefixMisMatch(t *testing.T) {
+	tests := []struct {
+		desc          string
+		nodeAddress   Address
+		nodeLength    uint32
+		searchAddress Address
+		searchLength  uint32
+	}{
+		{
+			desc:          "full bytes, mismatch in last byte",
+			nodeAddress:   _a("10::"),
+			nodeLength:    112,
+			searchAddress: _a("10::1:0"),
+			searchLength:  128,
+		},
+		{
+			desc:          "full bytes, mismatch in earlier byte",
+			nodeAddress:   _a("10::"),
+			nodeLength:    112,
+			searchAddress: _a("10:1::"),
+			searchLength:  128,
+		},
+		{
+			desc:          "full bytes, mismatch in first byte",
+			nodeAddress:   _a("10::"),
+			nodeLength:    112,
+			searchAddress: _a("11::"),
+			searchLength:  128,
+		},
+		{
+			desc:          "mismatch in partial byte",
+			nodeAddress:   _a("10::"),
+			nodeLength:    120,
+			searchAddress: _a("10::8000"),
+			searchLength:  128,
+		},
+		{
+			desc:          "only one partial byte",
+			nodeAddress:   Address{},
+			nodeLength:    15,
+			searchAddress: _a("2::"),
+			searchLength:  16,
+		},
+		{
+			desc:          "only one full byte",
+			nodeAddress:   Address{},
+			nodeLength:    16,
+			searchAddress: _a("10::"),
+			searchLength:  32,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			var trie *trieNode
+
+			trie, err := trie.Insert(Prefix{
+				tt.nodeAddress,
+				tt.nodeLength,
+			}, nil)
+			assert.Nil(t, err)
+			assert.True(t, trie.active())
+			assert.True(t, trie.isValid())
+			assert.Equal(t, 1, trie.height())
+
+			assert.Nil(t, trie.Match(Prefix{
+				tt.searchAddress,
+				tt.searchLength,
+			}))
+		})
+	}
+}
+
+func TestMatchSimplePrefixMatch(t *testing.T) {
+	tests := []struct {
+		desc        string
+		nodeAddress Address
+		nodeLength  uint32
+	}{
+		{
+			desc:        "full bytes, mismatch in last byte",
+			nodeAddress: _a("10::"),
+			nodeLength:  112,
+		},
+		{
+			desc:        "full bytes, mismatch in earlier byte",
+			nodeAddress: _a("10::"),
+			nodeLength:  112,
+		},
+		{
+			desc:        "full bytes, mismatch in first byte",
+			nodeAddress: _a("10::"),
+			nodeLength:  112,
+		},
+		{
+			desc:        "mismatch in partial byte",
+			nodeAddress: _a("10::"),
+			nodeLength:  120,
+		},
+		{
+			desc:        "only one full byte",
+			nodeAddress: Address{},
+			nodeLength:  16,
+		},
+		{
+			desc:        "partial byte",
+			nodeAddress: Address{uint128{0xfe00000000000000, 0}},
+			nodeLength:  15,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			var trie *trieNode
+
+			key := Prefix{
+				tt.nodeAddress,
+				tt.nodeLength,
+			}
+			trie, err := trie.Insert(key, nil)
+			assert.Nil(t, err)
+			assert.True(t, trie.isValid())
+			assert.Equal(t, 1, trie.height())
+
+			assert := assert.New(t)
+			assert.Equal(trie, trie.Match(key))
+		})
+	}
+}
+
+func TestMatchPartialByteMatches(t *testing.T) {
+	tests := []struct {
+		nodeAddress uint128
+		nodeLength  uint32
+	}{
+		{
+			nodeAddress: uint128{0x8000000000000000, 0},
+			nodeLength:  1,
+		},
+		{
+			nodeAddress: uint128{0xc000000000000000, 0},
+			nodeLength:  2,
+		},
+		{
+			nodeAddress: uint128{0xe000000000000000, 0},
+			nodeLength:  3,
+		},
+		{
+			nodeAddress: uint128{0xf000000000000000, 0},
+			nodeLength:  4,
+		},
+		{
+			nodeAddress: uint128{0xf800000000000000, 0},
+			nodeLength:  5,
+		},
+		{
+			nodeAddress: uint128{0xfc00000000000000, 0},
+			nodeLength:  6,
+		},
+		{
+			nodeAddress: uint128{0xfe00000000000000, 0},
+			nodeLength:  7,
+		},
+		{
+			nodeAddress: uint128{0xff00000000000000, 0},
+			nodeLength:  8,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d", tt.nodeLength), func(t *testing.T) {
+			var trie *trieNode
+
+			key := Prefix{
+				Address{tt.nodeAddress},
+				tt.nodeLength,
+			}
+			trie, err := trie.Insert(key, nil)
+			assert.Nil(t, err)
+			assert.True(t, trie.active())
+			assert.True(t, trie.isValid())
+			assert.Equal(t, 1, trie.height())
+
+			assert := assert.New(t)
+			assert.Equal(trie, trie.Match(Prefix{
+				// Always use 0xff to ensure that extraneous bits in the data are ignored
+				Address{uint128{0xff00000000000000, 0}},
+				tt.nodeLength,
+			}))
+
+			// byte with 0 in the last bit to match based on nodeLength
+			var mismatch uint128
+			mismatch = uint128{0xff00000000000000, 0}.and(uint128{0x8000000000000000, 0}.rightShift(int(tt.nodeLength - 1)).complement())
+
+			assert.Nil(trie.Match(Prefix{
+				// Always use a byte with a 0 is the last matched bit
+				Address{mismatch},
+				tt.nodeLength,
+			}))
+		})
+	}
+}
+
+func TestInsertOverlapping(t *testing.T) {
+	tests := []struct {
+		desc    string
+		a, b, c Prefix
+	}{
+		{
+			desc: "32 and 48",
+			a:    Prefix{_a("10:200::"), 32},
+			b:    Prefix{_a("10:200:20::"), 48},
+			c:    Prefix{_a("10:200:20::"), 128},
+		},
+		{
+			desc: "34 and 60",
+			a:    Prefix{_a("10:200::"), 34},
+			b:    Prefix{Address{uint128{0x0ac800e000000000, 0}}, 60},
+			c:    Prefix{Address{uint128{0x0ac800f800000000, 0}}, 127},
+		},
+		{
+			desc: "0 and 32",
+			a:    Prefix{Address{uint128{0, 0}}, 0},
+			b:    Prefix{_a("10::"), 16},
+			c:    Prefix{_a("10:10::"), 32},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			// This test inserts the three given nodes in the order given and
+			// checks that they are found in the resulting trie
+			subTest := func(first, second, third Prefix) func(t *testing.T) {
+				return func(t *testing.T) {
+					var trie *trieNode
+
+					trie, err := trie.Insert(first, nil)
+					assert.Nil(t, err)
+					assert.NotNil(t, trie.Match(first))
+					assert.True(t, trie.isValid())
+					assert.Equal(t, 1, trie.height())
+
+					trie, err = trie.Insert(second, nil)
+					assert.Nil(t, err)
+					assert.NotNil(t, trie.Match(second))
+					assert.True(t, trie.isValid())
+					assert.Equal(t, 2, trie.height())
+
+					trie, err = trie.Insert(third, nil)
+					assert.Nil(t, err)
+					assert.NotNil(t, trie.Match(third))
+					assert.True(t, trie.isValid())
+					assert.Equal(t, 3, trie.height())
+				}
+			}
+			t.Run("forward", subTest(tt.a, tt.b, tt.c))
+			t.Run("backward", subTest(tt.c, tt.b, tt.a))
+
+			// This sub-test tests that a node cannot be inserted twice
+			insertDuplicate := func(key Prefix) func(t *testing.T) {
+				return func(t *testing.T) {
+					var trie *trieNode
+
+					trie, err := trie.Insert(key, nil)
+					assert.Nil(t, err)
+					assert.True(t, trie.active())
+					assert.NotNil(t, trie)
+					assert.True(t, trie.isValid())
+					assert.Equal(t, 1, trie.height())
+
+					dup := key
+					newTrie, err := trie.Insert(dup, nil)
+					assert.NotNil(t, err)
+					assert.Equal(t, trie, newTrie)
+					assert.True(t, trie.isValid())
+					assert.Equal(t, 1, trie.height())
+				}
+			}
+			t.Run("duplicate a", insertDuplicate(tt.a))
+			t.Run("duplicate b", insertDuplicate(tt.b))
+		})
+	}
+}
+
+func TestInsertDisjoint(t *testing.T) {
+	tests := []struct {
+		desc        string
+		a, b, super Prefix
+	}{
+		{
+			desc:  "first bit",
+			a:     Prefix{Address{uint128{0, 0}}, 1},
+			b:     Prefix{_a("8000::"), 1},
+			super: Prefix{Address{uint128{0, 0}}, 0},
+		},
+		{
+			desc:  "thirty-third bit",
+			a:     Prefix{_a("A000:2240::"), 33},
+			b:     Prefix{_a("A000:2240:8000::"), 33},
+			super: Prefix{_a("A000:2240::"), 32},
+		},
+		{
+			desc:  "partial b bit",
+			a:     Prefix{_a("A000:2240::"), 47},
+			b:     Prefix{_a("A000:2240:8::"), 47},
+			super: Prefix{_a("A000:2240::"), 44},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			subTest := func(first, second Prefix) func(t *testing.T) {
+				// This test inserts the two given nodes in the order given and
+				// checks that they are both found in the resulting trie
+				return func(t *testing.T) {
+					var trie *trieNode
+
+					trie, err := trie.Insert(first, nil)
+					assert.Nil(t, err)
+					assert.Equal(t, trie.Prefix, first)
+					assert.True(t, trie.isValid())
+					assert.Equal(t, 1, trie.height())
+
+					trie, err = trie.Insert(second, nil)
+					assert.Nil(t, err)
+					assert.NotNil(t, trie.Match(second))
+					assert.True(t, trie.isValid())
+					assert.Equal(t, 2, trie.height())
+
+					assert.Nil(t, trie.Match(tt.super))
+
+					// The following are testing a bit more of the internals
+					// than I normally do.
+					assert.False(t, trie.active())
+					assert.Equal(t, tt.super, trie.Prefix)
+
+					// insert an active node the same as `super` to turn it active
+					trie, err = trie.Insert(tt.super, nil)
+					assert.Nil(t, err)
+					assert.NotNil(t, trie.Match(tt.super))
+					assert.True(t, trie.isValid())
+					assert.Equal(t, 2, trie.height())
+				}
+			}
+			t.Run("forward", subTest(tt.a, tt.b))
+			t.Run("backward", subTest(tt.b, tt.a))
+		})
+	}
+}
+
+func TestInsertMoreComplex(t *testing.T) {
+	tests := []struct {
+		desc string
+		keys []Prefix
+	}{
+		{
+			desc: "mix disjoint and overlapping",
+			keys: []Prefix{
+				Prefix{Address{uint128{0, 0}}, 0},
+				Prefix{Address{uint128{0xff00000000000000, 0}}, 8},
+				Prefix{Address{uint128{0xfe00000000000000, 0}}, 8},
+				Prefix{Address{uint128{0xffff000000000000, 0}}, 16},
+				Prefix{Address{uint128{0xfffe000000000000, 0}}, 16},
+				Prefix{Address{uint128{0xffff000000000000, 0}}, 17},
+				Prefix{Address{uint128{0xfffe800000000000, 0}}, 17},
+				Prefix{Address{uint128{0xfffe800000000000, 0}}, 18},
+				Prefix{Address{uint128{0xffffb00000000000, 0}}, 18},
+				Prefix{Address{uint128{0xfffebf0000000000, 0}}, 24},
+				Prefix{Address{uint128{0xffffbe0000000000, 0}}, 24},
+				Prefix{Address{uint128{0xfffffffffffffebf, 0}}, 64},
+				Prefix{Address{uint128{0xffffffffffffffbe, 0}}, 64},
+				Prefix{Address{uint128{0xffffffffffffffff, 0xffffffff00000000}}, 96},
+				Prefix{Address{uint128{0xffffffffffffffff, 0xfffffffe00000000}}, 96},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			t.Run("forward", func(t *testing.T) {
+				var trie *trieNode
+
+				for _, key := range tt.keys {
+					var err error
+					trie, err = trie.Insert(key, nil)
+					assert.Nil(t, err)
+					assert.NotNil(t, trie.Match(key))
+				}
+			})
+			t.Run("backward", func(t *testing.T) {
+				var trie *trieNode
+
+				for i := len(tt.keys); i != 0; i-- {
+					var err error
+					key := tt.keys[i-1]
+
+					trie, err = trie.Insert(key, nil)
+					assert.Nil(t, err)
+					assert.NotNil(t, trie.Match(key))
+				}
+			})
+		})
+	}
 }
 
 func TestContains(t *testing.T) {
@@ -217,6 +666,53 @@ func TestContains(t *testing.T) {
 				assert.Equal(t, tt.a.length != tt.b.length, reversed)
 			})
 		})
+	}
+}
+
+func TestSuccessivelyBetter(t *testing.T) {
+	var trie *trieNode
+
+	keys := []Prefix{
+		Prefix{_a("2001:224::0d24"), 0},
+		Prefix{_a("2001:224::0d24"), 1},
+		Prefix{_a("2001:224::0d24"), 8},
+		Prefix{_a("2001:224::0d24"), 16},
+		Prefix{_a("2001:224::0d24"), 24},
+		Prefix{_a("2001:224::0d24"), 32},
+		Prefix{_a("2001:224::0d24"), 40},
+		Prefix{_a("2001:224::0d24"), 48},
+		Prefix{_a("2001:224::0d24"), 56},
+		Prefix{_a("2001:224::0d24"), 64},
+		Prefix{_a("2001:224::0d24"), 72},
+		Prefix{_a("2001:224::0d24"), 80},
+		Prefix{_a("2001:224::0d24"), 88},
+		Prefix{_a("2001:224::0d24"), 96},
+		Prefix{_a("2001:224::0d24"), 104},
+		Prefix{_a("2001:224::0d24"), 112},
+		Prefix{_a("2001:224::0d24"), 120},
+		Prefix{_a("2001:224::0d24"), 128},
+	}
+
+	// Add successively more specific keys to the trie and assert that exact
+	// matches are returned when appropriate and non-exact, but longest matches
+	// are returned for the rest.
+	for index, key := range keys {
+		var err error
+		trie, err = trie.Insert(key, nil)
+		assert.Nil(t, err)
+		assert.Equal(t, int64(index+1), trie.NumNodes())
+		assert.True(t, trie.isValid())
+		assert.Equal(t, index+1, trie.height())
+
+		for i, searchKey := range keys {
+			node := trie.Match(searchKey)
+			assert.NotNil(t, node)
+			if i <= index {
+				assert.Equal(t, searchKey, node.Prefix)
+			} else {
+				assert.Equal(t, keys[index], node.Prefix)
+			}
+		}
 	}
 }
 
